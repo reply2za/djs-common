6c65e685a2cd491fa85501747bea2b43d6527706
Zain <reply2zain@gmail.com>
2023-04-08 20:20:02 -0400

    refactor: make CommandHandler naming consistent

--- src/main/entry.ts
+++ src/main/entry.ts
@@ -1,6 +1,6 @@
 import {EmbedBuilderLocal} from "./utils/EmbedBuilderLocal";
 import {MessageEventCore} from "./utils/types";
 import {ILogger} from "./interfaces/ILogger";
-import {CommandHandler} from "./handlers/commandHandler";
+import {CommandHandler} from "./handlers/CommandHandler";
 
 export {EmbedBuilderLocal, ILogger, CommandHandler, MessageEventCore};
